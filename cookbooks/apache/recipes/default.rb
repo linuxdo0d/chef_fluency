ac1a90e1a800630bdd9803bceb3a5bc239785f11
“DM” <dmcfarland@snaplogic.com>
2017-06-26 16:08:40 -0700

    updating cookbooks

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,7 +4,15 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
+if node['platform_family'] == 'debian'
+	package = 'apache2'
+elsif node['platform_family'] == 'rhel'
+	package = 'httpd'
+end
+
+
 package 'apache2' do
+	package_name package
 	action :install
 end
 
